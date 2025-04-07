55c743a7193f816eeb8e5394a70753e894e600ef
Pritam Biswas <pritamb@nvidia.com>
2025-04-06 23:54:17 -0700

    add nvcf support

--- garak/generators/nvcf.py
+++ garak/generators/nvcf.py
@@ -7,6 +7,12 @@ import json
 import logging
 import time
 from typing import List, Union
+import requests, io
+import base64
+import io
+from io import BytesIO
+from PIL import Image as _PILImage
+# from vlm_client import VLMClient
 
 import backoff
 import requests
@@ -16,9 +22,78 @@ from garak.exception import ModelNameMissingError, BadGeneratorException
 from garak.generators.base import Generator
 
 
+## GLOBALS 
+NVCF_API_TOKEN = "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"
+NVCF_FUNCTION_NAME="85638347-c3ff-4eda-a8e5-1a03fb2dfd1c"
+NVCF_MODEL_NAME="cosmos-nemotron-4B"
+
+def base64_image(img):
+    """
+    Resize an image encoded as a Base64 string.
+    Args:
+    base64_string (str): Base64 string of the original image.
+    size (tuple): Desired size of the image as (width, height).
+    Returns:
+    str: Base64 string of the resized image.
+    """
+    buffered = io.BytesIO()
+    img.save(buffered, format=img.format)
+    return base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+def ping_server(prompt, payload, url, headers):
+    global NVCF_API_TOKEN
+    global NVCF_FUNCTION_NAME
+    global NVCF_MODEL_NAME
+
+    url = "https://api.nvcf.nvidia.com/v2/nvcf/pexec/functions/" + NVCF_FUNCTION_NAME
+    token=NVCF_API_TOKEN
+
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "accept": "application/json",
+        "Content-Type": "application/json"
+    }
+    text = prompt["text"]
+
+    text = text.replace('<image>', '')
+    # print(' text prompt : ')
+    # print(text)
+    
+    # print("####")
+    image_filename = prompt["image"]
+    # print(image_filename)    
+    payload = {
+    "model": NVCF_MODEL_NAME,
+    "temperature": 1.0,
+    "top_p": 0.0,  
+    "top_k": 1.0,  
+    "max_tokens": 512,
+    "messages": [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": text},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{base64_image(_PILImage.open(image_filename))}"
+                    }
+                }
+            ]
+        }
+    ]
+    }
+    response = requests.post(url, headers=headers, json=payload)
+    # print('Status code ')
+    # print(response.status_code)
+    # assert response.status_code == 500
+    return response
+
 class NvcfChat(Generator):
     """Wrapper for NVIDIA Cloud Functions Chat models via NGC. Expects NVCF_API_KEY environment variable."""
 
+    # ENV_VAR = "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"
     ENV_VAR = "NVCF_API_KEY"
     DEFAULT_PARAMS = Generator.DEFAULT_PARAMS | {
         "temperature": 0.2,
@@ -39,9 +114,15 @@ class NvcfChat(Generator):
     def __init__(self, name=None, config_root=_config):
         self.name = name
         self._load_config(config_root)
+        self.local_mode = True
+        # self.vlm_client = VLMClient()
+        print('VLM model loaded ..')
+        # print(' ## Loaded COnfig ##')
         self.fullname = (
             f"{self.generator_family_name} {self.__class__.__name__} {self.name}"
         )
+        self.seed = _config.run.seed
+        # self.seed = 9999
 
         if self.name is None:
             raise ModelNameMissingError(
@@ -59,8 +140,15 @@ class NvcfChat(Generator):
             "Authorization": f"Bearer {self.api_key}",
             "Accept": "application/json",
         }
+        # print(' NVCF CHAT constructor end ## ')
+
+    def _prepare_prompt(self, prompt):
+        # print(' enter parent prepare prompt')
+        return prompt
 
     def _build_payload(self, prompt) -> dict:
+        # print('enter parent build payload')
+        prompt = self._prepare_prompt(prompt)
 
         payload = {
             "messages": [{"content": prompt, "role": "user"}],
@@ -89,27 +177,46 @@ class NvcfChat(Generator):
         max_value=70,
     )
     def _call_model(
-        self, prompt: str, generations_this_call: int = 1
+        self, prompt: str | List[dict], generations_this_call: int = 1
     ) -> List[Union[str, None]]:
-
+        # print(" ### Enter NVCF Chat call model () ## ")
         session = requests.Session()
 
         payload = self._build_payload(prompt)
 
+        # print(' Generated Payload ##')
+        # print(payload)
+
         ## NB config indexing scheme to be deprecated
-        config_class = f"nvcf.{self.__class__.__name__}"
-        if config_class in _config.plugins.generators:
-            if "payload" in _config.plugins.generators[config_class]:
-                for k, v in _config.plugins.generators[config_class]["payload"].items():
-                    payload[k] = v
+        # config_class = f"nvcf.{self.__class__.__name__}"
+        # if config_class in _config.plugins.generators:
+        #     if "payload" in _config.plugins.generators[config_class]:
+        #         for k, v in _config.plugins.generators[config_class]["payload"].items():
+        #             payload[k] = v
 
         if self.seed is not None:
             payload["seed"] = self.seed
+        # print(payload.keys())
 
         request_time = time.time()
         logging.debug("nvcf : payload %s", repr(payload))
-        response = session.post(self.invoke_uri, headers=self.headers, json=payload)
+        # print(' BEfore post request ')
+        # print(self.invoke_uri)
+        # print(self.headers)
+        prompt['text'] = prompt['text'].replace('<image>', '')
 
+        # print(prompt)
+        if self.local_mode:
+                response = self.vlm_client.ping_client(text = prompt['text'], images = [prompt['image']])
+                return [response]
+    # print(response)
+        response = ping_server(prompt, payload, self.invoke_uri, self.headers)
+        # print(payload)
+        # response = session.post(self.invoke_uri, headers=self.headers, json=payload)
+        # print("## response code ## ")
+        # print(response.status_code)
+        if response.status_code == 500:
+            logging.debug(response.json())
         while response.status_code == 202:
             if time.time() > request_time + self.timeout:
                 raise TimeoutError("NVCF Request timed out")
@@ -147,9 +254,100 @@ class NvcfChat(Generator):
 
         else:
             response_body = response.json()
-
+            # print(' Received response')
             return self._extract_text_output(response_body)
 
+
+class VLMChat(NvcfChat):
+    """ Class for VLM model text + image -> text """
+
+    # DEFAULT_PARAMS =  {
+    #     "suppressed_params": {"n", "frequency_penalty", "presence_penalty", "stop"},
+    #     "max_image_len": 180_000,
+    # }
+
+    modality = {"in": {"text", "image"}, "out": {"text"}}
+
+    def __init__(self, nvcf_function_name = None, model_name = None, config_root=_config):
+        # print(' ## Enter VLMChat init () ')
+        model_name = "Nemovision-4B-v2-instruct"
+        model_name =  "cosmos-nemotron-4B"
+        self.model_name = model_name
+        nvcf_function_name = "69053ae4-f69e-412f-9f83-c36de1b3ff17"
+        nvcf_function_name = "85638347-c3ff-4eda-a8e5-1a03fb2dfd1c"
+        super().__init__(name = nvcf_function_name, config_root = _config) 
+
+        self.local_mode = False
+         # Call the parent class constructor
+        # print(' ## Exit VLMChat init () ')
+        # self.age = age
+
+    def _prepare_prompt(self, prompt):
+        # print(" #nter prepare prompt ")
+        if isinstance(prompt, str):
+            prompt = {"text": prompt, "image": None}
+
+        text = prompt["text"]
+        image_filename = prompt["image"]
+        # print(image_filename)
+        # if self.local_mode:
+        #     processed_prompt = {"text": text, "image": image_filename}
+        # else:
+        #     processed_prompt = {"text": text, "image": base64_image(_PILImage.open(image_filename))}
+        processed_prompt = {"text": text, "image": image_filename}
+        return processed_prompt
+        pass ## prep text + image input here 
+
+    def _build_payload(self, prompt) -> dict:
+        # print('enter build payload')
+        prompt = self._prepare_prompt(prompt)
+
+        # payload = {
+        # "messages": [{"content": prompt, "role": "user"}],
+        # "temperature": self.temperature,
+        # "top_p": self.top_p,
+        # "max_tokens": self.max_tokens,
+        # "stream": False,
+        # }
+
+        # for k, v in self.extra_params.items():
+        #     payload[k] = v
+        # print(prompt.keys())
+        # print(prompt['text'])
+        # print(' #Enter VLMChat payload build ## ')
+        if self.local_mode:
+            payload = prompt
+        else:
+            payload = {
+                "model": self.model_name,
+                "temperature": 1.0,
+                "top_p": 0.0,  
+                "top_k": 1.0,  
+                "max_tokens": 100,
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": [
+                            {"type": "text", "text":  prompt['text']},
+                            {
+                                "type": "image_url",
+                                "image_url": {
+                                    "url": f"data:image/jpeg;base64,{prompt['image']}"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        # print(' Exit payload build ##')
+        return payload
+        pass # prep pakcet with inference params 
+        # print("Response:", response.json()['choices'][0]['message']['content'][0]['text'])
+
+    def _extract_text_output(self, response) -> str:
+        # print(' Extracted output : ')
+        # print(response['choices'][0]['message']['content'])
+        return [response['choices'][0]['message']['content']]
 
 class NvcfCompletion(NvcfChat):
     """Wrapper for NVIDIA Cloud Functions Completion models via NGC. Expects NVCF_API_KEY environment variables."""
